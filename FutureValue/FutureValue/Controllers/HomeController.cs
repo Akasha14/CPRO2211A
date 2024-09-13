fdd52bf1a07735ed1c2e10bef4a5da0a27175798
Akasha14 <ardan.kasha@rdpolytech.ca>
2024-09-12 19:41:51 -0600

    FutureValue App (Activity 1)

---
+++ FutureValue/FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+
+}
